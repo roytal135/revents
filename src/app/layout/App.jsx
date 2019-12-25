273988f36e204212227398178ea65f39430dc4d3
Roy Tal <roytal@h-MacBook-Pro-sl-Roy-2.local>
2019-12-25 22:56:54 +0200

    Initial Commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+    <div>
+      <h1>Hello you are at home</h1>
+    </div>
+  );
+}
+}
+export default App;
